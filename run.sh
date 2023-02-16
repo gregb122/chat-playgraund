e19ddf9f316a3b11db94e23500632f934bb85418
Grzegorz Bielecki <grzegorz.bielecki9@gmail.com>
2023-02-16 15:54:14 +0100

    Modify run script

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 set -o errexit
 set -o nounset
 
-uvicorn app:app --host "0.0.0.0" --port $PORT --reload --ws 'auto' \
---loop 'auto' --workers 4
+uvicorn main:app --host "0.0.0.0" --port $PORT --reload --ws 'auto' \
+--loop 'auto' --worke
+
 
 exec "$@"
